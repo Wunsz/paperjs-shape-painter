545cd0fe8c42fa461e70c01f83ec2ac1a16d0dcc
Bartosz Jabłoński <bjablonski@codeinthecup.pl>
2020-07-15 13:54:54 +0200

    Updates

---
+++ precache-manifest.d0d9a8d7b0f5f5c1d6a703fbc95cfb20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d603b367811b413911f2dd76659eeb7",
+    "url": "/paperjs-shape-painter/index.html"
+  },
+  {
+    "revision": "15b5fe46b23561500e24",
+    "url": "/paperjs-shape-painter/static/css/main.4c29094a.chunk.css"
+  },
+  {
+    "revision": "70bbaea3dec2ac1f292f",
+    "url": "/paperjs-shape-painter/static/js/2.3eafda62.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/paperjs-shape-painter/static/js/2.3eafda62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15b5fe46b23561500e24",
+    "url": "/paperjs-shape-painter/static/js/main.9d594ee7.chunk.js"
+  },
+  {
+    "revision": "043fb30aaab514460323",
+    "url": "/paperjs-shape-painter/static/js/runtime-main.4c54a15a.js"
+  }
+]);
